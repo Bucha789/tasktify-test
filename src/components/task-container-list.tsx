cce01d5a57f0153ff8adc833160a3f953f148791
JuanJos789 <216w0654@zongolica.tecnm.mx>
2025-03-12 15:14:47 -0600

    feat(drag-and-drop): Add Drag and Drop funcionality on list and board view

--- src/components/task-container-list.tsx
+++ src/components/task-container-list.tsx
@@ -1,23 +1,83 @@
-import { Container, Typography } from "@mui/material";
-import { Task, TaskStatus } from "../store/slices/task-slice";
+import { Box, Container, Typography } from "@mui/material";
+import { changeTaskStatus, Task, TaskStatus } from "../store/slices/task-slice";
 import { groupTasksByStatus } from "../utils/group";
 import { TaskList } from "./task-list";
-import { Fragment } from "react";
+import { getNearestElement } from "../utils/dom";
+import { useDispatch } from "react-redux";
+import { getElements } from "../utils/dom";
+
+
 const taskLabels = {
   [TaskStatus.TODO]: 'To Do',
   [TaskStatus.IN_PROGRESS]: 'In Progress',
   [TaskStatus.COMPLETED]: 'Completed',
 }
 const statusOrder = [TaskStatus.TODO, TaskStatus.IN_PROGRESS, TaskStatus.COMPLETED]
+
+
+const ListSection = ({
+  status,
+  tasks,
+}: {
+  status: TaskStatus
+  tasks: Task[]
+}) => {
+  const dispatch = useDispatch();
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    handleClearHighlights();
+    const elements = getElements(`[data-column="${tasks[0].status}"]`);
+    const { element } = getNearestElement(event, elements);
+    const task = JSON.parse(event.dataTransfer.getData('task'));
+    dispatch(changeTaskStatus({
+      id: task.id,
+      status: tasks[0].status as TaskStatus,
+      beforeId: element.dataset.before
+    }));
+  }
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    handleHighlightIndicator(event);
+  }
+
+  const handleDragLeave = () => {
+    handleClearHighlights();
+  }
+  const handleClearHighlights = (elements?: HTMLElement[]) => {
+    const indicators = elements || getElements(`[data-column="${tasks[0].status}"]`);
+    indicators.forEach((element) => {
+      element.style.opacity = "0";
+      element.style.height = "16px";
+    });
+  }
+  const handleHighlightIndicator = (e: React.DragEvent<HTMLDivElement>) => {
+    const elements = getElements(`[data-column="${tasks[0].status}"]`);
+    handleClearHighlights(elements);
+    const el = getNearestElement(e, elements);
+    el.element.style.opacity = "1";
+    el.element.style.height = "24px";
+  }
+  return (
+    <Box
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+    >
+      <Typography variant="h6" marginBottom={2}>{taskLabels[status as TaskStatus]} {tasks.length > 0 && ` (${tasks.length})`}</Typography>
+      <TaskList tasks={tasks} />
+    </Box>
+  )
+}
+
+
+
 export const TaskContainerList = ({ tasks }: { tasks: Task[] }) => {
   const groupedTasks = groupTasksByStatus(tasks, statusOrder);
+
   return (
     <Container maxWidth="md">
-      {Object.entries(groupedTasks).map(([status, tasks]) => (<Fragment key={status}>
-        <Typography variant="h6" marginBottom={2}>{taskLabels[status as TaskStatus]} {tasks.length > 0 && ` (${tasks.length})`}</Typography>
-        <TaskList key={status} tasks={tasks} />
-      </Fragment>
-      ))}
+      {Object.entries(groupedTasks).map(([status, tasks]) => (<ListSection key={status} status={status as TaskStatus} tasks={tasks} />))}
     </Container>
   )
 }
