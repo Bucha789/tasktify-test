5b579066560d2275d381f7036bd893e51ec18889
JuanJos789 <216w0654@zongolica.tecnm.mx>
2025-03-11 20:47:23 -0600

    feat(animations): Add animations when you delete

--- src/components/task-container.tsx
+++ src/components/task-container.tsx
@@ -1,13 +1,21 @@
 import { useSelector } from "react-redux"
 import { RootState } from "../store"
 import { TaskItem } from "./task-item"
-import { Container } from "@mui/material"
+import { Container, Box } from "@mui/material"
+import { AnimatePresence } from "framer-motion"
 
 export const TaskContainer = () => {
   const tasks = useSelector((state: RootState) => state.tasks.addedTasks)
-  return <Container maxWidth="md">
-    {tasks.map((task) => (
-      <TaskItem key={task.id} {...task} />
-    ))}
-  </Container>
+  
+  return (
+    <Container maxWidth="md">
+      <Box sx={{ position: 'relative' }}>
+        <AnimatePresence mode="popLayout" initial={false}>
+          {tasks.map((task) => (
+            <TaskItem key={task.id} {...task} />
+          ))}
+        </AnimatePresence>
+      </Box>
+    </Container>
+  )
 }
